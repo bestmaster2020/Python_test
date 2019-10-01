2802cea7050bcf76e5844c574217d1116621a0b2
H201701072 <H201701072@du.edu.om>
2019-10-01 14:01:00 +0400

    first PYthon

---
+++ test.py
@@ -0,0 +1,7 @@
+
+def add ():
+    a=input(" Enter A : ")
+    b=input(" Enter B : ")
+
+    print("Ans =",int(a)+int(b))
+add()
---
+++ test2.py
@@ -0,0 +1,13 @@
+def add(a,b):
+    
+  
+    if a>b:
+            
+        a=input(" Enter A : ")
+        b=input(" Enter B : ")
+
+        print(" a is grater than b")
+    elif:
+        print(" b is grater than a")   
+
+add()         

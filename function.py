88b9b08fb3939093f01f31a8a02810cc4adc801d
H201701072 <H201701072@du.edu.om>
2019-10-03 14:04:50 +0400

    add new code

---
+++ function.py
@@ -0,0 +1,25 @@
+
+
+#make varibale to collect
+
+a=input(" n1 :")
+b=input(" n2 :")
+
+
+def add():
+    print("Add Ans =",int(a)+int(b))
+
+
+add()
+
+c=input(" n3 :")
+
+def sub():
+    print("sub Ans =",int(int(a)+int(b))-int(c))
+
+sub()
+
+
+
+
+
---
+++ function2.py
@@ -0,0 +1,12 @@
+
+gba=input(" GBA :")
+
+
+def reg():
+
+    if int(gba) < 70:
+        print(" { Sorry you can't register }")
+    else:
+        print(" { can register } ")
+
+reg()
